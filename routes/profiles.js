84b190826aa5473ae9fbb3f7a3d55b5a0749125d
abdelmanaf <ourotoumanaf6@gmail.com>
2021-10-18 14:01:47 -0400

    user is able to see their profile picture included name and email

--- routes/profiles.js
+++ routes/profiles.js
@@ -5,6 +5,9 @@ import * as profilesCtrl from "../controllers/profiles.js"
 const router = Router();
 
 router.get('/', isLoggedIn, profilesCtrl.index)
+router.get('/:id', isLoggedIn, profilesCtrl.show)
+
+
 
 
 
