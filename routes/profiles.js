99121321d7974f9834ca1769aeb83a9e6e83473e
abdelmanaf <ourotoumanaf6@gmail.com>
2021-10-18 15:07:18 -0400

    I move the lougout button insite my others links items

--- routes/profiles.js
+++ routes/profiles.js
@@ -5,8 +5,8 @@ import * as profilesCtrl from "../controllers/profiles.js"
 const router = Router();
 
 router.get('/', isLoggedIn, profilesCtrl.index)
-router.get('/:id', isLoggedIn, profilesCtrl.show)
 router.get('/:id/edit', profilesCtrl.edit)
+router.get('/:id', isLoggedIn, profilesCtrl.show)
 
 
 
